fabad410acbd8cb1e25e7711568c572b0baffb0a
root <root@DESKTOP-TRRF5IU>
2024-06-30 23:40:50 +0200

    app/Api/MoviesApi.php: Suppression clé en brut, ramener du .env

--- app/Api/MoviesApi.php
+++ app/Api/MoviesApi.php
@@ -10,7 +10,7 @@ class MoviesApi
 
         $response = $client->request('GET', 'https://api.themoviedb.org/3/trending/movie/'. $time_window .'?language=fr-FR', [
             'headers' => [
-                'Authorization' => 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMjJkNjNjZGRjMDY2ZDk5ZWQzZTgwNmQzMjY3MThjYSIsInN1YiI6IjYyNGVhNTRhYjc2Y2JiMDA2ODIzODc4YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.zuuBq1c63XpADl8SQ_c62hezeus7VibE1w5Da5UdYyo',
+                'Authorization' => 'Bearer '.env('MOVIE_DB_API_KEY'),
                 'accept' => 'application/json',
             ],
         ]);
@@ -26,7 +26,7 @@ class MoviesApi
 
         $response = $client->request('GET', 'https://api.themoviedb.org/3/movie/'. $id .'?language=fr_FR', [
             'headers' => [
-                'Authorization' => 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMjJkNjNjZGRjMDY2ZDk5ZWQzZTgwNmQzMjY3MThjYSIsInN1YiI6IjYyNGVhNTRhYjc2Y2JiMDA2ODIzODc4YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.zuuBq1c63XpADl8SQ_c62hezeus7VibE1w5Da5UdYyo',
+                'Authorization' => 'Bearer '.env('MOVIE_DB_API_KEY'),
                 'accept' => 'application/json',
             ],
         ]);
